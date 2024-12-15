1a216d4e50093bf01ffe7bb5e120beb9fec87634
antrobot1234 <antrobot1234@gmail.com>
2024-12-15 13:18:19 -0800

    improved logger class functionality

--- logger.py
+++ logger.py
@@ -7,14 +7,26 @@ class bcolor:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
-
+def color_print(msg,color,newline=True):
+        if newline:
+            print(f"{color}{msg}{bcolor.ENDC}")
+        else:
+            print(f"{color}{msg}{bcolor.ENDC}",end='')
 class Logger:
-    def __init__(self,do_info=True,do_warn=True):
+    def __init__(self,do_info=True,do_warn=True,do_debug=False):
         self.do_info = do_info
         self.do_warn = do_warn
-    def info(self,msg):
+        self.do_debug = do_debug
+    
+    def info(self,msg,newline=True):
         if self.do_info:
-            print(f"{bcolor.OKGREEN}INFO: {bcolor.ENDC}{msg}")
+            color_print(msg,bcolor.OKGREEN,newline)
     def warn(self,msg):
         if self.do_warn:
-            print(f"{bcolor.WARNING}WARNING: {bcolor.ENDC}{msg}")
+            color_print(msg,bcolor.WARNING)
+    def debug(self,msg,newline=True):
+        if self.do_debug:
+            color_print(msg,bcolor.OKBLUE,newline)
+    def error(self,msg):
+        color_print(msg,bcolor.FAIL)
+        exit
