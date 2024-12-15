91a835bf9c368d9db9fa67ab02a7205e4451bf1c
antrobot1234 <antrobot1234@gmail.com>
2024-12-15 13:06:00 -0800

    migrated advanced parameters to a seperate yaml

--- launch.bash
+++ launch.bash
@@ -5,7 +5,7 @@ source /path/to/your/virtual/environment/bin/activate
 
 # Set script directory and YAML list variables
 script_dir="/path/to/your/StableDiffusionProjects/sd-scripts"
-yaml_list=("base.yaml" "hyperparameters.yaml" "directories.yaml" "extras.yaml" "performance.yaml")
+yaml_list=("base.yaml" "hyperparameters.yaml" "directories.yaml" "extras.yaml" "performance.yaml", "advanced_parameters.yaml")
 
 # Launch the accelerate script with specified parameters
 accelerate launch --num_cpu_threads_per_process 8 main.py --script_dir "$script_dir" --yaml_list "${yaml_list[@]}"
