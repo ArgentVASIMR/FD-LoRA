0ae1fdb207ebd526fb1da025d160ed75c1046dcb
antrobot1234 <antrobot1234@gmail.com>
2024-12-15 13:03:47 -0800

    modified launch script args to reflect new default yaml names

--- launch.bat
+++ launch.bat
@@ -1,7 +1,7 @@
 call D:\StableDiffusionProjects\sd-scripts\venv\scripts\activate.bat
 
 set script_dir=D:\StableDiffusionProjects\sd-scripts
-set yaml_list=config.yaml hyperparameters.yaml directories.yaml options.yaml performance.yaml
+set yaml_list=base.yaml hyperparameters.yaml directories.yaml extras.yaml performance.yaml
 
 accelerate launch --num_cpu_threads_per_process 8 main.py --script_dir %script_dir% --yaml_list %yaml_list%
 pause
