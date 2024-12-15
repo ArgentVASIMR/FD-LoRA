df04002b7d9d97aaadba1e8fee0198e2d4087cf5
antrobot1234 <antrobot1234@gmail.com>
2024-12-15 13:00:37 -0800

    added code to handle the fact that yamls are now not in root

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     parser = get_parser()
     args = parser.parse_args()
     yaml_list = args.yaml_list
-    arg_dict = utils.stack_yamls(yaml_list)
+    arg_dict = utils.stack_yamls(yaml_list,'yamls')
     train(arg_dict, args.script_dir)
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,12 +1,15 @@
 import yaml
+import os
 def read_yaml(path: str) -> dict:
     with open(path, 'r') as f:
         return yaml.safe_load(f)
 def write_yaml(path: str, dict: dict) -> None:
     with open(path, 'w') as f:
         yaml.dump(dict, f)
-def stack_yamls(paths: list[str]) -> dict:
+def stack_yamls(paths: list[str],root_dir: str = None) -> dict:
     out = {}
+    if root_dir is not None:
+        paths = [os.path.join(root_dir,path) for path in paths]
     for path in paths:
         out.update(read_yaml(path))
     return out
